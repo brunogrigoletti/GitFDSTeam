62e54dd1d5c99911e01b39cfa727e0f72072e334
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2024-03-19 21:57:27 -0300

    Branch BrunoL criada

--- X.java
+++ X.java
@@ -1,5 +1,9 @@
 public class X {
     public static void main(String[] args) {
         System.out.println("Classe X criada.");
+
+        // Criei a branch CriaX apenas para criar a classe X. Agora, criei a branch BrunoL
+        // e estou adicionando este comentário para fazer novas alterações sobre esta nova
+        // versão do código.
     }
 }
